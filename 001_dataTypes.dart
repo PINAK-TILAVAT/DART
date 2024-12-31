8fbeb538546f19c6229fef76e2a9bb77aa3a28ed
PINAK <pinaktilavat0@gmail.com>
2024-12-31 07:12:46 +0530

    Dart files added

---
+++ 001_dataTypes.dart
@@ -0,0 +1,49 @@
+// Nummaric
+
+// whole number
+int intNum = -10;
+
+//floating nums
+double doubleNum = 5.5;
+
+//maybe int or double
+num numNum = 1;
+
+//bigInt : nums that int cant handle
+
+//------------------------------------------------------------
+
+//String
+
+String s = "I AM MUSIC"; //immutable
+
+//------------------------------------------------------------
+
+//Booleans
+
+bool tr = true;
+bool fls = true;
+
+//------------------------------------------------------------
+
+// LIST
+
+List<dynamic> names = [
+  'playboi Cartiii',
+  DateTime(2024, 12, 31),
+  "Cartii plzzz drop the album"
+];
+
+//------------------------------------------------------------
+
+// MAPS
+
+Map<String, dynamic> maps = {"name": "pinak", "age": 20};
+
+//------------------------------------------------------------
+
+// SETS
+
+Set<String> set = {"pinak", "tilavat"};
+
+//------------------------------------------------------------
